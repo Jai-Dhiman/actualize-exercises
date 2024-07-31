31a21bdd59224631ae4f02e50de10adac5736f7b
JaiDhiman <jaidhiman2000@gmail.com>
2024-07-31 14:59:15 -0700

    142.rb

---
+++ 142.rb
@@ -0,0 +1,26 @@
+# QUESTION 1
+# Write a program that starts with an array of strings, 
+# then prints out each string on separate lines using a while loop.
+
+names = ["John", "Jacob", "Jingleheimer", "Smith"]
+
+index = 0
+while index < 4
+    puts names[index]
+    index = index + 1 
+end
+
+
+# QUESTION 2
+# Write a program that starts with an array of numbers, 
+# then prints out each number times 3 using a while loop.
+
+number = [1, 3, 5, 7]
+
+index = 0
+while index < 4
+    puts number[index] * 3
+    index = index + 1
+end
+
+    
