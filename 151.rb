62568a4673b17597d06f955aeaa8a8e8db828cb4
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-01 14:12:18 -0700

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,32 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+
+def display_Pineapple
+    puts '__________.__                                   .__          '
+    puts '\______   \__| ____   ____ _____  ______ ______ |  |   ____  '
+    puts ' |     ___/  |/    \_/ _  \\__  \ \____ \\____ \|  | _/ __ \ '
+    puts ' |    |   |  |   |  \  ___/ / __ \|  |_> >  |_> >  |_\  ___/ '
+    puts ' |____|   |__|___|  /\___  >____  /   __/|   __/|____/\___  >'
+    puts '                  \/     \/     \/|__|   |__|             \/ '
+end
+
+display_Pineapple
+display_Pineapple
+display_Pineapple
+
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+  end
+
+  print_lyrics
