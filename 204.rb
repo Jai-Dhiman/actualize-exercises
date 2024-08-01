7ed7d5716563695ec714149746233a925211dd74
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-01 16:39:21 -0700

    Create 204.rb

---
+++ 204.rb
@@ -0,0 +1,10 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+#     https://docs.google.com/document/d/1XaPaG6QXSQEajkHE1noa5pMfa27qmegSOY_FY6zBoQA/copy  
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
+
+
+https://docs.google.com/document/d/1MwT5bWfDJZoCeysQ4hroH_wS_ORl8EFGe3QTtHbiLus/edit?usp=sharing
+
