0d6dfb402407dffa1f6dd64908ea02d31438b63d
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-01 16:46:58 -0700

    Create 211.rb

---
+++ 211.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+index = 0
+numbers = [3,6,8,4,7]
+while index < numbers.length
+    puts numbers[index]
+    index=index+1
+end
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#I messed up the syntax for creating an array, so thats tommorrows practice goal
+#I put numbers [3,6,8,4,7], instead of numbers = [3,6,8,4,7]
