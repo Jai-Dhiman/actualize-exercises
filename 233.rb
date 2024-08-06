f361d2bc260127b8127a6a2453eef4f68f4764ce
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-06 18:28:29 -0400

    233.rb

--- 233.rb
+++ 233.rb
@@ -4,18 +4,30 @@
 numbers = [4, 3, 1, 5]
 new_numbers = []
 index = 0
+=begin
 while index < numbers.length
   number = numbers[index]
   new_numbers.push(number + 5)
   index = index + 1
 end
-pp new_numbers
+=end
+numbers.length.times do
+  number = numbers[index]
+  new_numbers.push(number + 5)
+  index = index + 1
+end
+pp new_numbers  
 
+numbers.each do |number|
+  pp number + 5
+end
 
 # QUESTION 2
 # Fix the errors with the code below.
+
 colors = ["red", "green", "blue"]
-color.each do |colors|
-  puts color.upcase
-  index = index + 1
+colors.each do |colors|
+  puts colors.upcase
+  #index = index + 1
 end
+
