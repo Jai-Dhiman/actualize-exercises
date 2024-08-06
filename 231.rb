829f0efd483a56e4396c5f6d69b084cfb258d1ff
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-05 23:36:41 -0700

    231.rb

---
+++ 231.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Make a hash to store definitions for 2 different words. 
+#Then add a new word and definition and print the hash to see the result.
+
+definitions = {'cat' => 'furry animal','knife' => 'sharp kitchen tool'}
+definitions['chair'] = 'something to sit on'
+
+pp definitions
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Will practice methods tommorrow
+
+
+
