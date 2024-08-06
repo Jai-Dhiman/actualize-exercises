c56be0db7fe4e7e0b47c2d487db0be48b7679588
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-05 23:00:59 -0700

    Create 221.rb

---
+++ 221.rb
@@ -0,0 +1,24 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+=begin
+Write a method that takes in a number and returns the number as a string. Then run the method and print the result.
+Ruby solution
+=end
+
+def method1
+
+    num = gets.chomp
+    puts num.to_s
+    
+    end
+    
+method1 
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+
+#The only issue for me was missing the def command to define a method, but after looking up the syntax I was good
