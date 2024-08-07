7b285cfbbe0216f52447fd471bea3f0559790f93
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-07 16:42:43 -0400

    251.rb

---
+++ 251.rb
@@ -0,0 +1,11 @@
+#Make an array of hashes to store the title and author for 3 different books. Then print out the third book's author.
+
+books = [
+{Title: 'The Hitchhiker\'s Guide to the Galaxy', Author:'Douglas Adams' },
+{Title: 'Three Body Problem', Author: 'Liu Cixin'},
+{Title: 'Catch 22', Author: 'Joeseph Heller'}
+]
+
+puts books[2][:Author]
+
+#I did this one without looking at notes, so tommorrow i am going to focus on practicing Methods and Classes
