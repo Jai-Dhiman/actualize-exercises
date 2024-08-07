9f209d926efcbdf1c32df9a8ad2181141fdb9fd6
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-06 22:40:02 -0400

    242.rb

--- 242.rb
+++ 242.rb
@@ -2,9 +2,14 @@
 # Use a variable to store an array with three different words.
 # Then add one more word to the array and print the array on one line.
 
+words = ['blanket', 'table', 'sink']
+words.push('bookcase')
+pp words
 
 # QUESTION 2
 # Use a variable to store a hash with two different words and their definitions.
 # Then add one more word with its definition and print the hash on one line.
 
-
+definitions = {'water' => 'makes things wet', 'fire' => 'burns things'}
+definitions['air'] = 'lets us breathe'
+pp definitions
