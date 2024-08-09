91f76ec82e16bd59c68eafc4860aedd11f1b7eab
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-09 17:31:11 -0400

    334.rb

--- 334.rb
+++ 334.rb
@@ -11,23 +11,29 @@
 # The program will say: "apple was your most common word"
 # (That's because apple appeared in the user's list 4 times, more than any other word.)
 
-words = []
+
+#I want to create a hash that stores the numbers as the key, and the count as the value
+words = {}
+puts "Give 10 words (duplicates are okay): "
+
+#I want to create a loop that checks if the word is in the hash, if it is it adds one to the count value
 10.times do
-    puts "Give word: "
     word = gets.chomp
-    words.push(word)
+    #This if loop should check if the word already exists in the hash
+    if words.include?(word) == false
+        #If the word doesn't exist this will add the word, and add one to the value side
+       words[word] = 1
+    else
+        #hopefully this adds one to the count side of the words has (unsure on this part)
+        words[word] = words[word].to_i + 1
+    end
 end
 
-word1 = words.count(words[0])
-word2 = words.count(words[1])
-word3 = words.count(words[2])
-word4 = words.count(words[3])
-word5 = words.count(words[4])
-word6 = words.count(words[5])
-word7 = words.count(words[6])
-word8 = words.count(words[7])
-word9 = words.count(words[8])
-word10 = words.count(words[9])
+#check to see if hash stored properly
+#pp words
 
-count_array = [word1, word2, word3, word4, word5, word6, word7, word8, word9, word10]
-pp count_array
+#I want to find the greatest value on the count value sides and then store that into an array 
+common = words.max_by{|k,v| v}
+
+#print out final statement
+puts "#{common[0]} was your most common word, you typed it #{common[1]} times"
