0cc6f76f5285b0a7b5d787c289919f3fe188e921
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-08 23:51:51 -0400

    323.rb

---
+++ 323.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Create a program that multiplies by 2 until 100,000. 
+# That is, the program should print out: 1, 2, 4, 8, 16, 32, etc. until you get above 100,000.
+
+sum = 1
+while sum <= 100000
+    puts sum
+    sum = sum * 2
+end
