b4b57e19c82bb104d3ee392376b25a52797ceb3b
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-09 15:31:18 -0400

    update 333.rb

--- 333.rb
+++ 333.rb
@@ -11,11 +11,13 @@
 # This is because when dividing integers in Ruby, the result will be an integer. 
 
 puts "Give me 5 numbers: "
-num1 = gets.chomp
-num2 = gets.chomp
-num3 = gets.chomp
-num4 = gets.chomp
-num5 = gets.chomp
+numbers = []
+5.times do
+    num = gets.chomp
+    numbers.push(num.to_i)
+end
 
-avg = (num1.to_i + num2.to_i + num3.to_i + num4.to_i + num5.to_i) / 5
+sum = numbers.sum
+avg = sum / (numbers.length)
+
 puts "Average is: #{avg}"
