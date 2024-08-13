705ca9bd52cface25df995d18e3f262e76c3f248
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-13 17:32:57 -0400

    422.rb

---
+++ 422.rb
@@ -0,0 +1,11 @@
+# QUESTION 1
+# Write a method that takes in a string and a number n and returns the string repeated n times.
+# Then run the method and print the result.
+
+
+def string_repeat(string, num)
+    puts string * num
+end
+
+string_repeat("hello", 5)
+
