8ece78ed001d27c1c9368da5312f21be7b74b60f
JaiDhiman <jaidhiman2000@gmail.com>
2024-08-22 18:09:57 -0700

    543, 544

---
+++ 543.js
@@ -0,0 +1,27 @@
+// QUESTION 1
+// Write code to store an array of numbers.
+// The print out the last item in that array.
+
+let numbers = [5, 10, 15, 20, 25];
+
+console.log(numbers[numbers.length - 1]);
+
+
+
+// QUESTION 2
+// Write code to store a number in a variable.
+// Then write a condition that 
+// prints -1 if the number is less than 10, 
+// prints 1 if the number is greater than 10, 
+// and prints 0 if the number is equal to 10.
+
+let num = 12;
+
+if (num < 10) {
+    console.log(-1);
+} else if (num > 10) {
+    console.log(1);
+} else {
+    console.log(0);
+}
+
